185130ca709c47db8bd635dac67f49db8f3f2207
Dardania <70666490+dardaniaa0@users.noreply.github.com>
2023-01-19 14:02:24 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -51,3 +51,15 @@ function parseEnvValue(value, defaultValue) {
   if (value === false || value === 'false') return false;
   return value;
 }
+
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
