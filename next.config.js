41574653809284441f48b9629607e74225ed91c0
Dardania <70666490+dardaniaa0@users.noreply.github.com>
2023-01-19 14:28:04 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
-// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`,
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-}
-
 const indexSearch = require('./plugins/search-index');
 const feed = require('./plugins/feed');
 const sitemap = require('./plugins/sitemap');
